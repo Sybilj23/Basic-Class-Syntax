8dee76ec2f0fbe1a00c31c5f84e7ebf60a9320aa
sybil <jordan.sybil@hotmail.com>
2024-03-06 21:23:51 -0800

    person complete

--- classes/person.js
+++ classes/person.js
@@ -5,22 +5,22 @@ class Person {
     this.age = age;
   }
   introduce() {
-    return `Hi, I'm ${firstName} ${lastName}, and I'm ${age} years old.".`;
+    console.log( `Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
   }
   // Loop through the array, is the object is not an instance of person, throw an error. After looping through the whole thing, if they are all instances of person,
   // loop through again and call the introduce method on each object
 
   static introducePeople(array) {
-    for (let i = 0; i < array.length; i++) {
-      let ppl = array[i];
-      if(ppl instanceof Person){
-        return introduce(ppl)
-        
-      }
-      if (!Array.isArray(array)) {
-        throw new Error(`introducePeople only takes an array as an argument.`);
-
+    if (!Array.isArray(array)) {
+      throw new Error(`introducePeople only takes an array as an argument.`);
     }
+    for (let i = 0; i < array.length; i++) {
+      if(!(array[i] instanceof Person)) {
+        throw new Error(`All items in array must be Person class instances.`);
+      }
+    }
+
+    array.forEach((person) => person.introduce());
   }
 }
 /****************************************************************************/
